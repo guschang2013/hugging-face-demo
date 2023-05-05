3158022c371c9fed1bc6b782d87439561854d3d2
guschang2013 <77599534+guschang2013@users.noreply.github.com>
2023-05-05 15:53:33 +0000

    fix app

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-
 # create an interface for the model
 with gr.Interface(predict, "textbox", "text") as interface:
     interface.launch()
